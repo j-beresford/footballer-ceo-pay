cf7501ac6d782eea119d1eb652ed4aacfe16ce4f
Justin Beresford <j-beresford9>
2020-09-07 10:06:35 +0100

    First upload - webscrapes salary data by PL team and converts all to single dataframe

---
+++ player_wages.R
@@ -0,0 +1,40 @@
+rm(list=ls())
+
+library(rvest)
+library(dplyr)
+
+site<-"https://www.spotrac.com/epl"
+page<-"payroll/"
+
+team<-c("arsenal-fc", "aston-villa-fc", "brighton-hove-albion", "burnley-fc", 
+        "chelsea-fc", "crystal-palace", "everton-fc", "fulham-fc", "leeds-united-fc",
+        "leicester-city", "liverpool-fc", "manchester-city-fc", "manchester-united-fc",
+        "newcastle-united-fc", "sheffield-united-fc", "southampton-fc",
+        "tottenham-hotspur-fc", "west-bromwich-albion-fc", "west-ham-united-fc",
+        "wolverhampton-wanderers-fc")
+
+
+urls<-paste(site, team, page, sep="/")
+
+
+h<-lapply(urls, read_html)
+names(h)<-team
+
+table_maker<-function(t){
+  t <- t %>% html_nodes("table")
+  t <- t[[1]]%>% html_table
+  }
+
+t<-lapply(h, table_maker)
+
+names(t)<-team
+
+names(t[[1]])
+
+t<-lapply(t, setNames, c("Player", "Position", "Age", "empty", 
+                         "Annual Salary", "Weekly Salary"))
+
+
+wages<-do.call(bind_rows, t)
+wages<-wages%>%select(-empty)
+
