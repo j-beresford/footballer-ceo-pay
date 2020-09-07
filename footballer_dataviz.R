0ffd9045b6e636b1ba84dde4a81e3e4d851c40c7
Justin Beresford <j-beresford9>
2020-09-07 10:37:49 +0100

    Data exploration for PL player wages

---
+++ footballer_dataviz.R
@@ -0,0 +1,25 @@
+source("player_wages.R")
+
+library(ggplot2)
+library(scales)
+
+wages<-wages_table%>%
+  select(-empty)%>%
+  filter(AnnualSalary!="-")%>%
+  mutate(AnnualSalary=str_remove_all(AnnualSalary,"£"))%>%
+  mutate(AnnualSalary=str_remove_all(AnnualSalary,","))%>%
+  mutate(AnnualSalary=as.numeric(AnnualSalary))%>%
+  mutate(WeeklySalary=str_remove_all(WeeklySalary,"£"))%>%
+  mutate(WeeklySalary=str_remove_all(WeeklySalary,","))%>%
+  mutate(WeeklySalary=as.numeric(WeeklySalary))
+
+
+wages%>%
+  ggplot(aes(AnnualSalary))+
+  geom_histogram()+
+  scale_x_continuous(labels=comma)+
+  theme_minimal()+
+  labs(x="Annual Salary", 
+       y="Number of players", 
+       title="Distribution of player wages)")
+
